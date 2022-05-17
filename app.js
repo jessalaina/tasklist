e7d073496f3737d392a86f8ca557bffc4015ec01
jessalaina <jessalaina90@gmail.com>
2022-05-17 19:48:51 -0400

    Add functionality of button and enter key to add tasks to list

---
+++ app.js
@@ -0,0 +1,40 @@
+const listGroup = document.querySelector('ul');
+const btn = document.querySelector('.btn');
+const input = document.querySelector('input');
+
+function addLis() {
+    // create li element
+    const lis = document.createElement('li');
+    // create text node for li
+    const text = document.createTextNode(input.value);
+    //create checkbox 
+    let checkbox = document.createElement('input');
+        checkbox.type = 'checkbox';
+        checkbox.value = 1;
+        checkbox.name = 'todo[]';
+    // append checkbox to li
+    lis.appendChild(checkbox);
+    // append text node to li node
+    lis.appendChild(text);
+    // append li to ul
+    listGroup.appendChild(lis);
+    // remove input value
+    input.value = '';
+}
+
+
+// click button to add task to list
+btn.addEventListener('click', function() {
+    if(input.value.length != 0) {
+        if (input.value.length != 0) {
+            addLis();
+        } 
+    }
+})
+
+// keypress to add task to list
+document.addEventListener('keypress', function(e) {
+    if(e.key === 'Enter') {
+        addLis();
+    }
+})
